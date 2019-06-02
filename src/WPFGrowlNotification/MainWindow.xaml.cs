96431db05beb7b943fab117a1b29c3942a119b6e
behzad <behzad.khosravifar@gmail.com>
2019-06-02 11:00:28 +0430

    first init

---
+++ src/WPFGrowlNotification/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFGrowlNotification
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private const double TopOffset = 20;
+        private const double LeftOffset = 380;
+        private readonly GrowlNotifiactions _growlNotifications = new GrowlNotifiactions();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _growlNotifications.Top = SystemParameters.WorkArea.Top + TopOffset;
+            _growlNotifications.Left = SystemParameters.WorkArea.Left + SystemParameters.WorkArea.Width - LeftOffset;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var rand = new Random().NextDouble() * 4;
+            if (rand <= 1)
+                _growlNotifications.AddNotification(new Notification { Title = "Message #1", ImageUrl = "pack://application:,,,/Resources/notification-icon.png", Message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." });
+            else if (rand <= 2)
+                _growlNotifications.AddNotification(new Notification { Title = "Message #2", ImageUrl = "pack://application:,,,/Resources/microsoft-windows-8-logo.png", Message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." });
+            else if (rand <= 3)
+                _growlNotifications.AddNotification(new Notification { Title = "Message #3", ImageUrl = "pack://application:,,,/Resources/facebook-button.png", Message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." });
+            else if (rand <= 4)
+                _growlNotifications.AddNotification(new Notification { Title = "Message #4", ImageUrl = "pack://application:,,,/Resources/Radiation_warning_symbol.png", Message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." });
+        }
+
+        protected override void OnClosed(System.EventArgs e)
+        {
+            _growlNotifications.Close();
+            base.OnClosed(e);
+        }
+
+        private void WindowLoaded1(object sender, RoutedEventArgs e)
+        {
+            //this will make minimize restore of notifications too
+            //growlNotifications.Owner = GetWindow(this);
+        }
+    }
+}
---
+++ src/WPFGrowlNotification/Notification.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace WPFGrowlNotification
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        private string _message;
+        public string Message
+        {
+            get => _message;
+
+            set
+            {
+                if (_message == value) return;
+                _message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        private int _id;
+        public int Id
+        {
+            get => _id;
+
+            set
+            {
+                if (_id == value) return;
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string _imageUrl;
+        public string ImageUrl
+        {
+            get => _imageUrl;
+
+            set
+            {
+                if (_imageUrl == value) return;
+                _imageUrl = value;
+                OnPropertyChanged("ImageUrl");
+            }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Notifications : ObservableCollection<Notification> { }
+}
